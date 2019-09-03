4f941a1e3eaeb7bdba4cd48a0b3feee8ddd0c372
Matthew Zamora <mzamora@supernus.com>
2019-09-03 17:48:02 -0400

    Add test typehint

--- src/Tests/DependencyInjection/ConfigurationTest.php
+++ src/Tests/DependencyInjection/ConfigurationTest.php
@@ -19,7 +19,7 @@ class ConfigurationTest extends TestCase
     /**
      * {@inheritdoc}
      */
-    public function setUp()
+    public function setUp(): void
     {
         $this->processor = new Processor();
     }
